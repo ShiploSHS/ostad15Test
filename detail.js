5d22bbadff0e6bed7037b167ef80def1641130cb
ShiploSHS <shiplo.shs@gmail.com>
2025-02-16 12:57:14 +0600

    details js dot

--- detail.js
+++ detail.js
@@ -1,4 +1,4 @@
-import products from '/products.js';
+import products from './products.js';
 import cart from './cart.js';
 
 let listProduct = document.getElementById('listProduct');
@@ -6,7 +6,7 @@ let app = document.getElementById('app');
 let temporaryContent = document.getElementById('temporaryContent');
 
 const loadTemplate = () => {
-    fetch('/template.html')
+    fetch('./template.html')
     .then(response => response.text())
     .then(html => {
         app.innerHTML = html;
@@ -39,7 +39,7 @@ const initApp = () => {
         let newProduct = document.createElement('div');
         newProduct.classList.add('item');
         newProduct.innerHTML = 
-        `<a href="/detail.html?id=${product.id}">
+        `<a href="./detail.html?id=${product.id}">
             <img src="${product.image}">
         </a>
         <h2>${product.name}</h2>
